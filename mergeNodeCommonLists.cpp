3b51ddcafb7b34d1f97a255df9ee5bb0715594ab
rutvik-k <30704408+rutvik-k@users.noreply.github.com>
2017-09-23 05:39:24 +0000

    Update mergeNodeCommonLists.cpp

--- mergeNodeCommonLists.cpp
+++ mergeNodeCommonLists.cpp
@@ -10,3 +10,39 @@ int FindMergeNode(Node *headA, Node *headB){
         headA=headA->next;
     }
 }
+
+
+
+
+/*
+   Find merge point of two linked lists
+   Node is defined as
+   struct Node
+   {
+       int data;
+       Node* next;
+   }
+*/
+
+int n(Node* A){
+    int count=0;
+    while(A!=NULL){
+        count++;
+        A=A->next;
+    }
+    return count;
+}
+int FindMergeNode(Node *headA, Node *headB){
+    // Complete this function
+    // Do not write the main method. 
+    for(int i=0;i<n(headA);i++){
+        for(int j=0;j<n(headB);j++){
+            if(headA==headB){
+                return headA->data;
+                break;
+            }
+            headB=headB->next;
+        }
+        headA=headA->next;
+    }
+}
